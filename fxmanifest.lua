095f0259f6913eff86af48e87136dcaac7300acb
shootprox <dyltelwesemann@gmail.com>
2021-12-16 17:19:04 +0100

    Git
    Git

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'xorzo#3928'
+description 'This resource makes it impossible to explode gas tanks with your car.'
+version '1.0.0'
+
+client_scripts {
+	'client.lua'
+}
